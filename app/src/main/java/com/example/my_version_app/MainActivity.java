83aba12fbdfb29aacda319f9ba61986dfbc44c96
Ivan Chibisov <knihaykinjan@gmail.com>
2019-09-25 20:03:26 +0300

    Add version changer and text view with this version

--- app/src/main/java/com/example/my_version_app/MainActivity.java
+++ app/src/main/java/com/example/my_version_app/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.my_version_app;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.TextView;
 import android.os.Bundle;
+import android.content.pm.PackageManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showVersion();
+    }
+    private void showVersion(){
+        String version;
+        try{
+            version = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+        } catch(PackageManager.NameNotFoundException e){
+            version = "unknown";
+        }
+        TextView versionTextView = findViewById(R.id.version_Tv);
+        versionTextView.setText("Application version: " + version);
     }
 }
